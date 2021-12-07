475d80a1119864fdd20c1aa8693c89018e9e5c9b
Arkodiusz <arecki358@gmail.com>
2021-12-07 21:43:52 +0100

    link to portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <footer>
-            2021 @ AJ
+            2021 @ <a href='https://arkodiusz.github.io/'>arkodiusz.github.io</a>
         </footer>
     )
 }
